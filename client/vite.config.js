2e2b6acf0c623a461af0f5f9c5403e151bd8f2e1
sobran rajak <sobran0621@gmail.com>
2024-10-21 20:27:52 +0530

    changes in vite.config and home

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,28 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        secure: false,
+  server:{      
+    proxy:{     
+      '/api':{
+        target:'http://localhost:3000',  
       },
     },
   },
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Ensures the output directory is 'dist'
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000, // Increase the chunk size warning limit if needed
-  },
-});
+})
